5d17a3e136eacb0642bfeceb27c2f51770f2fa32
aurea10 <srta_ferr@hotmail.com>
2019-09-18 21:27:27 -0300

    Aula07

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'etec-base';
+  title = 'Olá Galerivis!';
+  foto ='favicon.ico'
 }
